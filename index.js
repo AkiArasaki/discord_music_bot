53a638fb4380b3576159866942c4530dcce41543
AkiArasaki <96545911+AkiArasaki@users.noreply.github.com>
2023-03-12 19:02:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -52,18 +52,6 @@ class Bot {
                 guildId: msg.guild.id,
                 adapterCreator: msg.guild.voiceAdapterCreator
             });
-            this.connection[msg.guildId].on('stateChange', (oldState, newState) => {
-                const oldNetworking = Reflect.get(oldState, 'networking');
-                const newNetworking = Reflect.get(newState, 'networking');
-
-                const networkStateChangeHandler = (oldNetworkState, newNetworkState) => {
-                    const newUdp = Reflect.get(newNetworkState, 'udp');
-                    clearInterval(newUdp?.keepAliveInterval);
-                }
-
-                oldNetworking?.off('stateChange', networkStateChangeHandler);
-                newNetworking?.on('stateChange', networkStateChangeHandler);
-            });
             this.player[msg.guildId] = createAudioPlayer();
             this.connection[msg.guildId].subscribe(this.player[msg.guildId]);
         }
